55f744f73933e00ce05cbcf6f89538bba53b11b2
Kareem Shehata <kareem@shehata.ca>
2024-02-11 11:02:06 +0800

    Added checks that strings are the correct type (i.e. kanji and kana not swapped)

--- check_dupes.py
+++ check_dupes.py
@@ -1,5 +1,22 @@
 import sys
 import csv
+import re
+import string
+
+kanji_re = re.compile(r"[\s\u3000-\u30ff\u4e00-\u9fff]+")
+kana_re = re.compile(r"[\s\u3000-\u30ff]+")
+english_re = re.compile(r"[\sa-zA-Z0-9\u3000-\u303f\u2000-\u206f"
+                        + re.escape(string.punctuation)
+                        + r"]+")
+
+def is_kana(s):
+    return kana_re.fullmatch(s) is not None
+
+def is_kanji(s):
+    return kanji_re.fullmatch(s) is not None
+
+def is_english(s):
+    return english_re.fullmatch(s) is not None
 
 def check_dupes(item, all_items, name, filename, row):
     if len(item) > 0 and item in all_items:
@@ -23,6 +40,14 @@ for filename in sys.argv[1:]:
             kana = r[cols_map["kana"]]
             kanji = r[cols_map["kanji"]]
             english = r[cols_map["english"]]
+            if not is_kana(kana):
+                print(f"Invalid kana: {kana}, file: {filename}, row: {r}")
+            if len(kanji) > 0 and not is_kanji(kanji):
+                print(f"Invalid kanji: {kanji}, file: {filename}, row: {r}")
+            if len(kanji) > 0 and is_kana(kanji):
+                print(f"Kanji is kana: {kanji}, file: {filename}, row: {r}")
+            if not is_english(english):
+                print(f"Invalid english: {english}, file: {filename}, row: {r}")
             check_dupes(kana, all_kana, "kana", filename, r)
             check_dupes(kanji, all_kanji, "kanji", filename, r)
             check_dupes(english, all_english, "english", filename, r)
